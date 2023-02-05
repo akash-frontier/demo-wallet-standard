7c6cf4c919ca2fc57900616298c80b95dc1c2811
akash-frontier <akash@frontier.xyz>
2023-02-05 18:27:50 +0530

    updated package name

--- src/initialize.ts
+++ src/initialize.ts
@@ -1,7 +1,7 @@
 import { registerWallet } from './register.js';
-import { GhostWallet } from './wallet.js';
-import type { Ghost } from './window.js';
+import { DemoWalletWallet } from './wallet.js';
+import type { DemoWallet } from './window.js';
 
-export function initialize(ghost: Ghost): void {
-    registerWallet(new GhostWallet(ghost));
+export function initialize(demoWallet: DemoWallet): void {
+    registerWallet(new DemoWalletWallet(demoWallet));
 }
